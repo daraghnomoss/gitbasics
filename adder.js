309995242708a0e1c6b32812fe2021ef6b05a8d5
Daragh Byrne <daragh@nomoss.co>
2019-08-16 16:39:26 +1000

    Back to original

--- adder.js
+++ adder.js
@@ -4,11 +4,7 @@ console.log(gimmeFive());
 console.log(gimmeNothing());
 
 function gimmeFive() {
-<<<<<<< HEAD
-    return 5;
-=======
-    return 4 + 1;
->>>>>>> 477cd349f2f0113403e8d92c1443bd83f4faee24
+    return 3 + 2:;
 }
 
 function gimmeNothing() {
